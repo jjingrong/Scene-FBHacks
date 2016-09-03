47dc2cf237b042c104608216084cbe8ecca3b22b
Ace2196 <abdulla.contractor@gmail.com>
2016-09-03 21:31:57 +0800

    empty state page

--- chrome-ext/js/angularApp.js
+++ chrome-ext/js/angularApp.js
@@ -1,9 +1,5 @@
 var myApp = angular.module('sceneApp',[]);
 
-myApp.controller('StateController', ['$scope', function($scope) {
-  $scope.empty = true
-}]);
-
 myApp.controller('GreetingController', ['$scope', function($scope) {
-  $scope.greetings = ['Hello','Hola'];
+  $scope.greetings = [];
 }]);
