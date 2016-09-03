535cac5424eaabfdf156f103be6d649693b1ed44
JingRong ( <jing_rong92@hotmail.com>
2016-09-04 02:48:25 +0800

    Selected functionality

--- chrome-ext/js/angularApp.js
+++ chrome-ext/js/angularApp.js
@@ -13,6 +13,22 @@ myApp.controller('ItemsController', ['$scope', 'GetShowProducts', function($scop
     }, 1500);
 
     }
+	// @param: id of item pressed 
+	$scope.itemCellPressed = function(_id) {
+		for (var i = 0; i < $scope.items.length; i++) {
+			// Object found
+			if ($scope.items[i].id == _id) {
+				// Toggle selected
+				if ($scope.items[i].selected) {
+					$scope.items[i].selected = 0
+					console.log($scope.items[i])
+				} else {
+					$scope.items[i].selected = 1
+					console.log($scope.items[i])
+				}
+			}
+		}
+	}
 }]);
 
 myApp.service('GetShowProducts', ['$http', function($http){
