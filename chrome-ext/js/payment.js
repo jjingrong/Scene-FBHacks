2ea22227e79ec6310b1b83eb83b879be2ae3b865
Ace2196 <abdulla.contractor@gmail.com>
2016-09-04 07:53:55 +0800

    now can pay also

--- chrome-ext/js/payment.js
+++ chrome-ext/js/payment.js
@@ -1,40 +1,26 @@
-var script1 = document.createElement("script");
-script1.src = "https://js.stripe.com/v2/";
-$.getScript("https://js.stripe.com/v2/")
-//document.getElementsByTagName("head")[0].appendChild(script1);
-
-var script2 = document.createElement("script");
-script2.src = "https://checkout.stripe.com/checkout.js";
-$.getScript("https://checkout.stripe.com/checkout.js")
-//document.getElementsByTagName("head")[0].appendChild(script2);
-
-$("#customButton").on("click", function myFunction(){
-    console.log('paying');
-
+$(document).ready(function() {
+  $('#customButton').click(function(e) {
+    console.log('paying here')
     var handler = StripeCheckout.configure({
       key: 'pk_test_mkxhcUOMYR5aVoyu1KYsO0e2',
-      image: '../../icons/sceneLogo.png',
+      image: '/icons/sceneLogo.png',
       locale: 'auto',
       token: function(token) {
         // You can access the token ID with `token.id`.
         // Get the token ID to your server-side code for use.
       }
     });
-
-    $('#customButton').on('click', function(e) {
-      // Open Checkout with further options:
-      handler.open({
-        name: 'Demo Site',
-        description: '2 widgets',
-        amount: 2000
-      });
-      e.preventDefault();
+    // Open Checkout with further options:
+    handler.open({
+      name: 'Scene',
+      description: 'Payment Portal',
+      amount: parseFloat($('#total-price').text())*100
     });
+    e.preventDefault();
+  });
 
-    // Close Checkout on page navigation:
-    $(window).on('popstate', function() {
-      handler.close();
-    });
+  // Close Checkout on page navigation:
+  $(window).on('popstate', function() {
+    handler.close();
+  });
 });
-
-
